18fc884ee28e394262dbfd139f5aca06c8adadc1
unknown <manojkumarka99@gmail.com>
2019-09-22 21:15:20 +0530

    layouts and tab included from tabs

---
+++ resources/views/tabs/headline-sub-tab.blade.php
@@ -0,0 +1,34 @@
+
+
+      <h2 class="page-header"><i class="fa fa-book"></i> Activity Satus</h2>
+      <div class="row">
+        <!-- /.col (left) -->
+                <div class="col-md-12">
+          <div class="box box-solid">
+            
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                Background information
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                ISO Requiremets
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                A5.Information Secrity Policies
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                A6.Organization of Information Secrity
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col (right) -->
+
+      </div>
+      <!-- /.row -->
+
+      <!-- /.row -->
+      <!-- END PROGRESS BARS -->
---
+++ resources/views/tabs/headline-tab.blade.php
@@ -0,0 +1,113 @@
+
+
+      <h4 class=""><i class="fa fa-book"></i> Prpose and goals</h4>
+        <div class="col-md-12">
+          <button type="btton" class="btn pull-right btn-success">View Statemet of Applicability</button>
+        </div>
+      <div class="row">
+        <!-- /.col (left) -->
+                <div class="col-md-4">
+          <div class="">
+            
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                Background information
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                ISO Requiremets
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                A5.Information Secrity Policies
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                A6.Organization of Information Secrity
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col (right) -->
+        <div class="col-md-4">
+          <div class="">
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="progress">
+                <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="40" aria-valuemin="0" aria-valuemax="100" style="width: 40%">
+                  <span class="sr-only">40% Complete (success)</span>
+                </div>
+              </div>
+              <div class="progress">
+                <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="20" aria-valuemin="0" aria-valuemax="100" style="width: 20%">
+                  <span class="sr-only">20% Complete</span>
+                </div>
+              </div>
+              <div class="progress">
+                <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="60" aria-valuemin="0" aria-valuemax="100" style="width: 60%">
+                  <span class="sr-only">60% Complete (warning)</span>
+                </div>
+              </div>
+              <div class="progress">
+                <div class="progress-bar progress-bar-green" role="progressbar" aria-valuenow="80" aria-valuemin="0" aria-valuemax="100" style="width: 80%">
+                  <span class="sr-only">80% Complete</span>
+                </div>
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col (right) -->
+        <div class="col-md-4">
+          <div class="">
+            
+            <!-- /.box-header -->
+            <div class="box-body">
+              
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+
+      </div>
+      <!-- /.row -->
+
+      <!-- /.row -->
+      <!-- END PROGRESS BARS -->
+
+<div class="box-header with-border">
+      <h4 class=""></i> Activity Satus</h4>
+</div>
+      <div class="row">
+        <!-- /.col (left) -->
+                <div class="col-md-12">
+          <div class="">
+            
+            <!-- /.box-header -->
+            <div class="box-body">
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                Background information
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                ISO Requiremets
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                A5.Information Secrity Policies
+              </div>
+              <div class="progress" style="background-color: white;color: cornflowerblue;">
+                A6.Organization of Information Secrity
+              </div>
+            </div>
+            <!-- /.box-body -->
+          </div>
+          <!-- /.box -->
+        </div>
+        <!-- /.col (right) -->
+
+      </div>
+      <!-- /.row -->
+
+      <!-- /.row -->
+      <!-- END PROGRESS BARS -->

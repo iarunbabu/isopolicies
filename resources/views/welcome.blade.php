f58edd1c0c2059c15fb2aa59654ae5d8d43d3107
Manojk-1989 <manojkumarka99@gmail.com>
2019-09-26 09:05:15 +0530

    headline edited

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -110,8 +110,10 @@
    $(function () {
     // Replace the <textarea id="editor1"> with a CKEditor
     // instance, using default configuration.
+    
     CKEDITOR.replace('editor1')
     //bootstrap WYSIHTML5 - text editor
+    config.removeButtons = 'Anchor';
     $('.textarea').wysihtml5()
   })
 </script>

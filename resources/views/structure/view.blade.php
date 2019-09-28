fbeaca1c05294169564049fe2c81a3e8672a56cc
iarunbabu <bsa85a@gmail.com>
2019-09-29 01:11:35 +0530

    structure View edits

--- resources/views/structure/view.blade.php
+++ resources/views/structure/view.blade.php
@@ -225,9 +225,52 @@
                                                                 class="btn  btn-success btn-xs  pull-right">Add To-do
                                                         </button>
                                                     </div>
-
                                                     <p>no To-dos to display</p>
+                                                    <div class="row">
+                                                      <div class="col-md-12">
+                                                        <form class="form-horizontal">
+              <div class="box-body">
+                <div class="form-group">
+                  <label for="inputEmail3" class="col-sm-2 control-label">To do:</label>
+
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputEmail3" >
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Due:</label>
+
+                  <div class="col-sm-4">
+                    <input type="text" class="form-control pull-right" id="datepicker">
+                  </div>
+                </div>
+                <div class="form-group">
+                  <label for="inputPassword3" class="col-sm-2 control-label">Assigned to:</label>
+
+                  <div class="col-sm-10">
+                    <input type="text" class="form-control" id="inputPassword3" >
+                  </div>
+                </div>
+                
+              </div>
+              <!-- /.box-body -->
+              <div class="box-footer">
+                
+                <button type="button"  
+                                                                class="btn  btn-success btn-xs  pull-right">Create To-do
+                                                        </button>
+                                                        <div style="padding-right: 10px" class="pull-right"><a type="submit" >Cancel</a></div>
+                                                        
+              </div>
+              <!-- /.box-footer -->
+            </form>
+                                                        
+                                                        
+                                                        
+                                                      </div>
+                                                    </div>
                                                 </div>
+
 
                                                 <div class="box-header with-border">
                                                     <div class="row">
@@ -277,5 +320,12 @@
             </div>
         </div>
     </section>
+<script type="text/javascript">
+  $(function(){
+    $('#datepicker').datepicker({
+      autoclose: true
+    })
 
+  })
+</script>
 @stop
